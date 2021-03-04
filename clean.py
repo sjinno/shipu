55df8ce07000874d613dd5cfe8c269e6b55f24f7
Shohei Jinno <shohei.jinno@gmail.com>
2021-03-04 09:36:44 -0800

    Playing with Jinja2.

--- clean.py
+++ clean.py
@@ -1,4 +1,4 @@
-import os
+""" import os
 import requests
 from bs4 import BeautifulSoup
 
@@ -11,8 +11,37 @@ print(r.status_code)
 soup = BeautifulSoup(r.content, 'html.parser')
 recipe = soup.find(id='recipe')
 
-with open('output.html', 'w', encoding='utf-8') as f:
-    f.write(str(recipe.prettify()))
+
+txt = recipe.txt
+
+txt.replace('\n', ' ').split()
 
 
-os.system('xdg-open output.html')
+print(txt)
+
+
+# with open('output.html', 'w', encoding='utf-8') as f:
+#     f.write(str(recipe.prettify()))
+
+
+# os.system('xdg-open output.html')
+ """
+
+# from jinja2 import Template
+
+# template = Template('Hello, {{ name }}!')
+# print(template.render(name='John Doe'))
+
+
+from jinja2 import Environment, PackageLoader, select_autoescape, FileSystemLoader
+env = Environment(
+    loader=FileSystemLoader('./templates'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('mytemplate.html')
+
+# print(template.render(the='variables', go='here'))
+
+with open('out.html', 'w') as f:
+    f.write(template.render(the='variables', go='here'))
