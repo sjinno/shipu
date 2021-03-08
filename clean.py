4f1e56e0c731d723fc7de6b615491c04f82649dc
Shohei Jinno <shohei.jinno@gmail.com>
2021-03-07 21:55:59 -0800

    Update styles.

--- clean.py
+++ clean.py
@@ -1,50 +1,54 @@
+# Third party libraries:
 from bs4 import BeautifulSoup
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import requests
 
-
+# std
+import os
 import sys
 
 
+# Get environment varibale:
 url = sys.argv[1]  # 1 https://cookpad.com/recipe/1847041 - メンチカツ
-print(f'{url}')
+print(f"{url}")
+
 
 # SCRAPE DATA AND PARSE IT:
-r = requests.get(url, auth=('user', 'pass'))
-soup = BeautifulSoup(r.content, 'html.parser')
+r = requests.get(url, auth=("user", "pass"))
+if r.status_code == 200:
+    print("Success!")
+soup = BeautifulSoup(r.content, "html.parser")
 
-# CLEANING:
-title = soup.find(class_='recipe-title').text.strip()
-photo_url = soup.find(id='main-photo').img.attrs['src']
 
+# CLEANING CLEANING CLEANING:
+title = soup.find(class_="recipe-title").text.strip()
+photo_url = soup.find(id="main-photo").img.attrs["src"]
 # == CLEANING INGREDIENTS STARTS HERE ==
 names = [name.text for name in soup.find(
-    id='ingredients').find_all(class_='name')]
-amounts = [amount.text for amount in soup.find(
-    id='ingredients').find_all(class_='amount')]
+    id="ingredients").find_all(class_="name")]
+amounts = [
+    amount.text for amount in soup.find(id="ingredients").find_all(class_="amount")
+]
 ingredients = list(zip(names, amounts))
 # == CLEANING INGREDIENTS ENDS HERE ==
-
-steps = [step.text.strip() for step in soup.find_all(class_='step_text')]
-
+steps = [step.text.strip() for step in soup.find_all(class_="step_text")]
 # DONE CLEANING :)
 
 
+# Get template ready:
 env = Environment(
-    loader=FileSystemLoader('./templates'),
-    autoescape=select_autoescape(['html', 'xml'])
+    loader=FileSystemLoader("./templates"),
+    autoescape=select_autoescape(["html", "xml"]),
 )
-
-
-template = env.get_template('recipe_template.html')
+template = env.get_template("recipe_template.html")
 context = {
-    'title': title,
-    'photo_url': photo_url,
-    'ingredients': ingredients,
-    'steps': steps,
+    "title": title,
+    "photo_url": photo_url,
+    "ingredients": ingredients,
+    "steps": steps,
 }
 
 
-output_file = title + '.html'
-with open(output_file, 'w') as f:
+output_file = title + ".html"
+with open(output_file, "w") as f:
     f.write(template.render(context))
